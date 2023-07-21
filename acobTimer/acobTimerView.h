6da1ca9298bb1d6b2412d3cc1999eb69822414bd
Yaghoub ZargooshiFar <zargooshifar@outlook.com>
2023-07-21 11:22:11 +0330

    Initial Commit

---
+++ acobTimer/acobTimerView.h
@@ -0,0 +1,12 @@
+//
+//  acobTimerView.h
+//  acobTimer
+//
+//  Created by Yaghoub ZargooshiFar on 21.07.23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface acobTimerView : ScreenSaverView
+
+@end
---
+++ acobTimer/acobTimerView.m
@@ -0,0 +1,51 @@
+//
+//  acobTimerView.m
+//  acobTimer
+//
+//  Created by Yaghoub ZargooshiFar on 21.07.23.
+//
+
+#import "acobTimerView.h"
+
+@implementation acobTimerView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
